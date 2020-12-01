7c68407bd43ef3e4747f14a458cfd4511a7ab409
ChoiDongJoon <adong0808@naver.com>
2020-12-01 21:07:50 +0900

    Create Chapter9-Page33.cpp

---
+++ Chapter9-Page33.cpp
@@ -0,0 +1,32 @@
+﻿//전자정보공학부 IT융합전공 20192645 최동준
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char chString1[30] = "Hello", chString2[30];
+
+    strcpy_s(chString2, "Kim");
+    cout << chString1 << " " << chString2 << endl;
+    int nComp;
+    if (nComp = strcmp(chString1, chString2))
+    {
+        cout << nComp << "Two strings are different!";
+    }
+    else
+    {
+        cout << nComp << "Two strings are the same!";
+    }
+    cin >> chString2;
+    cout << chString2 << endl;
+
+    strcat_s(chString1, chString2);
+    cout << chString1 << endl;
+
+    cout << "Please enter a sentence with white spaces" << endl;
+    cin.ignore(30, '\n');
+    cin.getline(chString2, 30);
+    cout << chString2 << endl;
+}
+
