1280f27e5a8a7cb7de7077249825a737ec5ffdc7
ChoiDongJoon <adong0808@naver.com>
2020-12-01 21:05:09 +0900

    Create Chapter10-Page20.cpp

---
+++ Chapter10-Page20.cpp
@@ -0,0 +1,25 @@
+﻿// 전자정보공학부 IT융합전공 20192645 최동준
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int* p3, * p4;
+	p3 = new int;
+	p4 = new int;
+	
+	cout << "p3 : " << p3 << " p4 : " << p4 << endl;
+	cout << "*p3 : " << *p3 << " *p4 : " << *p4 << endl;
+
+	*p3 = 7;
+	*p4 = 8;
+
+	p3 = p4;
+	cout << "p3 : " << p3 << " p4 : " << p4 << endl;
+	cout << "*p3 : " << *p3 << " *p4 : " << *p4 << endl;
+
+	delete p3;
+	delete p4;
+
+
+}
